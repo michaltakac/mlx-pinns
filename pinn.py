527377be27ad8ebca59446da468e381853c388bd
Michal Takáč <takacmichal92@gmail.com>
2024-03-19 12:43:48 +0100

    feat: first working implementation

--- pinn.py
+++ pinn.py
@@ -1,3 +1,4 @@
+import os
 import mlx.core as mx
 import mlx.nn as nn
 import mlx.optimizers as optim
@@ -33,7 +34,6 @@ class Net(nn.Module):
 
     def __call__(self, x,t):
         inputs = mx.concatenate([x,t],axis=1)
-        print(inputs)
         layer1_out = mx.sigmoid(self.hidden_layer1(inputs))
         layer2_out = mx.sigmoid(self.hidden_layer2(layer1_out))
         layer3_out = mx.sigmoid(self.hidden_layer3(layer2_out))
@@ -44,6 +44,9 @@ class Net(nn.Module):
     
 ### (2) Model
 model = Net()
+if os.path.isfile("pinn.safetensors"):
+    model.load_weights("pinn.safetensors")
+
 mx.eval(model.parameters())
 optimizer = optim.Adam(learning_rate=1e-1)
 
@@ -52,24 +55,18 @@ def unet(x,t):
     return u.sum()
 
 ## PDE as loss function. Thus would use the network which we call as u_theta
-def pde_loss(x,t):
+def theta_pde(x,t):
     # u = net(x,t) # the dependent variable u is given by the network based on independent variables x,t
     ## Based on our f = du/dx - 2du/dt - u, we need du/dx and du/dt
 
     unet_grad_fn = mx.value_and_grad(unet)
-    print("unet_grad_fn",unet_grad_fn)
     u, grads = unet_grad_fn(x, t)
 
-    print("grads",grads)
     # u = net(x,t) # the dependent variable u is given by the network based on independent variables x,t
-    print("u", u)
 
     u_x = grads[0]
     u_t = grads[1]
-    print(u_x)
-    print(u_t)
     pde = u_x - mx.multiply(2, u_t) - u
-    print(pde)
     return pde
 
 ## Data from Boundary Conditions
@@ -83,40 +80,48 @@ t_bc = np.zeros((500,1))
 # compute u based on BC
 u_bc = 6*np.exp(-3*x_bc)
 
-### (3) Training / Fitting
-iterations = 20000
-previous_validation_loss = 99999999.0
+# collocation points
+x_collocation = np.random.uniform(low=0.0, high=2.0, size=(500,1))
+t_collocation = np.random.uniform(low=0.0, high=1.0, size=(500,1))
+all_zeros = np.zeros((500,1))
+all_ones = np.ones((500,1))
 
-def pinn_loss(model, x, t, pred, y):
+
+
+def pinn_loss(model, x_bc, t_bc, u_bc, x_collocation, t_collocation):
     # Loss based on boundary conditions
     pt_x_bc = mx.array(x_bc)
     pt_t_bc = mx.array(t_bc)
     pt_u_bc = mx.array(u_bc)
     
     net_bc_out = model(pt_x_bc, pt_t_bc) # output of u(x,t)
-    print("net_bc_out", net_bc_out)
     mse_u = nn.losses.mse_loss(net_bc_out, pt_u_bc)
-    print("MSE Loss:",mse_u)
     
     # Loss based on PDE
-    x_collocation = np.random.uniform(low=0.0, high=2.0, size=(500,1))
-    t_collocation = np.random.uniform(low=0.0, high=1.0, size=(500,1))
-    all_zeros = np.zeros((500,1))
-    
     pt_x_collocation = mx.array(x_collocation)
     pt_t_collocation = mx.array(t_collocation)
     pt_all_zeros = mx.array(all_zeros)
-    print(pt_x_collocation.shape,pt_t_collocation.shape,pt_all_zeros.shape)
+    pt_all_ones = mx.array(all_ones)
 
     # u = model(pt_x_collocation, pt_t_collocation) # the dependent variable u is given by the network based on independent variables x,t
-    f_out = pde_loss(pt_x_collocation, pt_t_collocation) # output of f(x,t)
+    f_out = theta_pde(pt_x_collocation, pt_t_collocation) # output of f(x,t)
+    f_out = f_out * pt_all_ones
     # loss_and_grad = nn.value_and_grad(mlp, l2_loss)
     # f_out = f(pt_x_collocation, pt_t_collocation, model) # output of f(x,t)
-    # mse_f = nn.losses.mse_loss(f_out, pt_all_zeros)
+    mse_f = nn.losses.mse_loss(f_out, pt_all_zeros)
     
     # Combining the loss functions
-    loss = mse_u# + mse_f
+    loss = mse_u + mse_f
     return loss
+
+
+loss_and_grad_fn = nn.value_and_grad(model, pinn_loss)
+
+
+### (3) Training / Fitting
+iterations = 1000
+previous_validation_loss = 99999999.0
+loss_values = []
 
 for epoch in range(iterations):
     # Loss based on boundary conditions
@@ -124,45 +129,32 @@ for epoch in range(iterations):
     pt_t_bc = mx.array(t_bc)
     pt_u_bc = mx.array(u_bc)
     
-    net_bc_out = model(pt_x_bc, pt_t_bc) # output of u(x,t)
-    print("net_bc_out", net_bc_out)
-    mse_u = nn.losses.mse_loss(net_bc_out, pt_u_bc)
-    print("MSE Loss:",mse_u)
-    
     # Loss based on PDE
-    x_collocation = np.random.uniform(low=0.0, high=2.0, size=(500,1))
-    t_collocation = np.random.uniform(low=0.0, high=1.0, size=(500,1))
-    all_zeros = np.zeros((500,1))
-    
-    
     pt_x_collocation = mx.array(x_collocation)
     pt_t_collocation = mx.array(t_collocation)
     pt_all_zeros = mx.array(all_zeros)
-    print(pt_x_collocation.shape,pt_t_collocation.shape,pt_all_zeros.shape)
 
-    # u = model(pt_x_collocation, pt_t_collocation) # the dependent variable u is given by the network based on independent variables x,t
-    f_out = pde_loss(pt_x_collocation, pt_t_collocation) # output of f(x,t)
-    # loss_and_grad = nn.value_and_grad(mlp, l2_loss)
-    # f_out = f(pt_x_collocation, pt_t_collocation, model) # output of f(x,t)
-    # mse_f = nn.losses.mse_loss(f_out, pt_all_zeros)
-    
-    # Combining the loss functions
-    loss = mse_u# + mse_f
-
-    loss_and_grad_fn = mx.value_and_grad(model, pde_loss)
+    loss, grads = loss_and_grad_fn(model, pt_x_bc, pt_t_bc, pt_u_bc, pt_x_collocation, pt_t_collocation)
 
     # Update the optimizer state and model parameters
     # in a single call
-    optimizer.update(model)
+    optimizer.update(model, grads)
 
     # Force a graph evaluation
     mx.eval(model.parameters(), optimizer.state)
-    
-    print(f"Epoch {epoch}: Loss {loss:.3f}")
 
+    loss_values.append(loss.tolist())
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+    if epoch % 50==0:
+        print(f"Epoch {epoch}: Loss {loss}")
+        model.save_weights("pinn.safetensors")
+
+losses_numpy = np.array(loss_values, copy=False)
+plt.plot(losses_numpy)
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 x=np.arange(0,2,0.02)
 t=np.arange(0,1,0.02)
@@ -174,13 +166,10 @@ t = np.ravel(ms_t).reshape(-1,1)
 pt_x = mx.array(x)
 pt_t = mx.array(t)
 pt_u = model(pt_x,pt_t)
-u=pt_u.data.cpu().numpy()
-ms_u = u.reshape(ms_x.shape)
+ms_u = pt_u.reshape(ms_x.shape)
 
 surf = ax.plot_surface(ms_x,ms_t,ms_u, cmap=cm.coolwarm,linewidth=0, antialiased=False)
              
-             
-
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
